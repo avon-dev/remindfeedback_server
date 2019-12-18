a4a7ae9995c5429805eaffa39ceb4409506a7ba1
jiunkoo <52573800+jiunkoo@users.noreply.github.com>
2019-12-18 21:05:17 +0900

    친구 검색 기능 추가

--- routes/friend.js
+++ routes/friend.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const { User, Feedback, Sequelize: { Op } } = require('../models');
+const { User, Friend } = require('../models');
 const passport = require('passport');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
@@ -10,29 +9,44 @@ const router = express.Router();
  * - parameter friend_id : 친구 추가할 회원 uuid
  */
 // friend search (친구 검색)
-router.get('/search', isLoggedIn, async function (req, res, next) {
+router.post('/search', isLoggedIn, async (req, res, next) => {
     try {
+        const user_email = req.user.email;
         const email = req.body.email;
-        const friend = await User.findOne({
-            attributes: ['user_uid'],
-        }, { // 이메일, 닉네임, 프로필사진 주소, 소개글
+
+        // 본인 이메일 검색 시 에러 출력
+        if (email == user_email) {
+            const result = new Object();
+            result.success = false;
+            result.data = 'NONE'
+            result.message = '사용자를 찾을 수 없습니다. 나 자신은 영원한 인생의 친구입니다.';
+            console.log(result);
+            return res.status(403).send(result);
+        }
+
+
+        console.log('친구 검색 요청', email);
+        console.log(req.body);
+        const exFriend = await User.findOne({
+            attributes: ['email', 'nickname', 'portrait', 'introduction'],
             where: {
                 email: email
             }
         });
-        // DB에서 친구 데이터를 꺼내오는데 성공한 경우
-        if (friend != null) {
+
+        // 친구 검색에 성공한 경우
+        if (exFriend) {
             const result = new Object();
             result.success = true;
-            result.data = friend
-            result.message = '친구 검색을 성공했습니다.';
+            result.data = exFriend;
+            result.message = '친구 검색에 성공했습니다.';
             console.log(result);
             return res.status(200).send(result);
         } else { // 실패한 경우
             const result = new Object();
             result.success = false;
             result.data = 'NONE'
-            result.message = '검색 결과를 찾을 수 없습니다. 이메일을 확인해주세요.';
+            result.message = '사용자를 찾을 수 없습니다. 이메일을 확인해주세요.';
             console.log(result);
             return res.status(403).send(result);
         }
@@ -46,7 +60,6 @@ router.get('/search', isLoggedIn, async function (req, res, next) {
 router.get('/create', isLoggedIn, async function (req, res, next) {
     try {
         const user_uid = req.user.user_uid;
-        const
     } catch (e) {
         console.error(e);
         return next(e);
