9f9b370e03dd8900d0cd3405b4a6de56f2837aa5
jiunkoo <52573800+jiunkoo@users.noreply.github.com>
2020-02-08 22:19:17 +0900

    board model 데이터 타입 변경

--- models/board.js
+++ models/board.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => (
             allowNull: true,
         }, 
         board_content: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: true,
         }, 
         board_file1: {
